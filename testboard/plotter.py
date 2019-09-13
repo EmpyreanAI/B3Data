cad2a7227d9d4a9bf26684b07f144a6011909f71
Renato Avellar Nobre <rekanobre@gmail.com>
2019-09-13 17:12:11 -0300

    Updated Keras

--- testboard/plotter.py
+++ testboard/plotter.py
@@ -27,15 +27,29 @@ class Plotter():
         plt.close('all')
 
     @staticmethod
-    def loss_epoch_plot(loss):
+    def loss_epoch_plot(loss_train):
         _, ax_plot = plt.subplots()
         hfont = {'fontname': 'monospace'}
         # ax_plot.set_title("{}-{}".format(stock, year), **hfont)
         ax_plot.set_xlabel('Epoch', **hfont)
         ax_plot.set_ylabel('Loss', **hfont)
-        ax_plot.plot(loss)
+        ax_plot.plot(loss_train)
         plt.show()
 
+    @staticmethod
+    def acc_epoch_plot(acc):
+        _, ax_plot = plt.subplots()
+        hfont = {'fontname': 'monospace'}
+        # ax_plot.set_title("{}-{}".format(stock, year), **hfont)
+        ax_plot.set_xlabel('Epoch', **hfont)
+        ax_plot.set_ylabel('Loss', **hfont)
+        ax_plot.plot(acc)
+        plt.show()
+    #
+    # def acc_loss_masterfuckingplot():
+    #     fig, (ax1, ax2) = plt.subplots(1, 2)
+    #     ax1.plot(x, y)
+    #     ax2.plot(x, -y)
         # filename = "./graphics/{}/{}/{}".format(stock, year, features)
         # os.makedirs(os.path.dirname(filename), exist_ok=True)
         # plt.savefig(filename)
