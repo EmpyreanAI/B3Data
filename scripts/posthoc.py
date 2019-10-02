d85635a8d5f57b130fc6aba08790012bdf0dbdf4
Khalil <khalilcarsten@gmail.com>
2019-10-02 16:52:43 -0300

    posthoc changes

--- scripts/posthoc.py
+++ scripts/posthoc.py
@@ -9,6 +9,8 @@ from testboard.simulator.plotter import Plotter
 from testboard.data_mining.smote import duplicate_data
 import pandas
 import numpy
+import os
+from tqdm import tqdm
 
 plotter = Plotter()
 cells = [1, 50, 80, 100, 150, 200]
@@ -21,27 +23,32 @@ def run():
     results_acc = []
     df = pandas.DataFrame()
 
-    for year in years:
-        stocks = Stocks(year=year, cod='PETR3', period=6)
-        dataset = stocks.selected_fields(fields)
-        dataset = duplicate_data(dataset)
-        for cell in cells:
+    for cell in tqdm(cells):
+        cells_results = []
+        for year in years:
+            stocks = Stocks(year=year, cod='PETR3', period=6)
+            dataset = stocks.selected_fields(fields)
+            dataset = duplicate_data(dataset)
             sequencial_kfold = SequencialKFold(n_split=10)
             acc, loss, conf_mat = sequencial_kfold.split_and_fit(data=dataset,
                                                                  look_back=0.5,
                                                                  cells=cell)
+            acc = [1,2,3,4,5,6,7]
             results_acc.append(acc)
-            df = df.append({'cells ' + str(cell): numpy.mean(acc)}, ignore_index=True)
+            cells_results.append(numpy.mean(acc))
 
-        outname = 'cells_experiment.csv'
-        outdir = '../results'
-        if not os.path.exists(outdir):
-            os.mkdir(outdir)
+        df_temp = pandas.DataFrame({'cells_' + str(cell): cells_results})
+        df = pandas.concat([df, df_temp], axis=1)
 
-        fullname = os.path.join(outdir, outname)
-        df.to_csv(fullname, mode='a')
+    outname = 'cells_experiment.csv'
+    outdir = '../results'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
 
-        return results_acc
+    fullname = os.path.join(outdir, outname)
+    df.to_csv(fullname, mode='a')
+
+    return results_acc
 
 results = run()
 
