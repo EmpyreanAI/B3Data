0807a571f5d0b72ae2d162cc954fc3dfe2498963
Khalil <khalilcarsten@gmail.com>
2020-01-21 02:32:43 -0300

    new posthoc

--- scripts/cells_experiment.py
+++ scripts/cells_experiment.py
@@ -13,9 +13,11 @@ plotter = Plotter()
 cells = [1, 50, 80, 100, 150, 200]
 fields = [CLOSING]
 
-stocks = Stocks(year=2014, cod='PETR3', period=5)
+stocks = Stocks(year=2014, cod=sys.argv[1], period=5)
 dataset = stocks.selected_fields(fields)
 dataset = duplicate_data(dataset)
+optmizer = sys.argv[2]
+batch_size = int(sys.argv[3])
 
 def run():
     """Nani."""
@@ -24,9 +26,11 @@ def run():
     for cell in cells:
         sequencial_kfold = SequencialKFold(n_split=6)
         acc, loss, conf_mat = sequencial_kfold.split_and_fit(data=dataset,
-                                                             cells=cell)
+                                                             cells=cell,
+                                                             optimizer=optmizer,
+                                                             batch_size=batch_size)
         results_acc.append(acc)
     return results_acc
 results = run()
 
-plotter.acc_cells_box_plot(data=results, stock='PETR3', year='2014')
+plotter.acc_cells_box_plot(data=results, stock=sys.argv[1], year='2014')
--- scripts/hyperopt_graphic.py
+++ scripts/hyperopt_graphic.py
@@ -1,45 +1,52 @@
 import pandas
 import matplotlib.pyplot as plt
 import matplotlib
+import numpy
 
 space = {
-    'batch_size': [1, 2, 32, 64, 128, 256],
-    'cells': [1, 50, 80, 100, 150, 200],
+    'batch_size': ["1", "2", "32", "64", "128", "256"],
+    'cells': ["1", "50", "80", "100", "150", "200"],
     'optimizers': ['sgd','adam','rmsprop'],
-    'look_back': [1, 3, 6, 9, 12],
+    'look_back': ["1", "3", "6", "9", "12"],
 }
 
 df = pandas.read_csv('../results/hyperopt_100_VALE3.csv')
 
-# plt.plot(df[['index']], df[['loss']])
-
-# import pdb; pdb.set_trace()
-cells = [int(space['cells'][int(df.iloc[i]['cells'][1])]) for i in range(len(df[['cells']]))]
-batch_size = [int(space['batch_size'][int(df.iloc[i]['batch_size'][1])]) for i in range(len(df[['batch_size']]))]
-look_back = [int(space['look_back'][int(df.iloc[i]['look_back'][1])]) for i in range(len(df[['look_back']]))]
+cells = [space['cells'][int(df.iloc[i]['cells'][1])] for i in range(len(df[['cells']]))]
+batch_size = [space['batch_size'][int(df.iloc[i]['batch_size'][1])] for i in range(len(df[['batch_size']]))]
+look_back = [space['look_back'][int(df.iloc[i]['look_back'][1])] for i in range(len(df[['look_back']]))]
 optimizers = [space['optimizers'][int(df.iloc[i]['optimizers'][1])] for i in range(len(df[['optimizers']]))]
+loss = [df.iloc[i]['loss'] for i in range(len(df[['optimizers']]))]
 
-# plt.plot(df[['index']], df[['loss']])
 
-ax = plt.subplot(1, 4, 1)
+ax = plt.subplot(2, 4, 1)
 ax.set_title('Cells')
-plt.yticks([1, 50, 80, 100, 150, 200])
+# plt.yticks(["1", "50", "80", "100", "150", "200"])
 plt.plot(df[['index']], cells, 'bo')
-ax = plt.subplot(1, 4, 2)
-ax.set_title('batch_size')
-plt.axis('normal')
-plt.yticks([1, 2, 32, 64, 128, 256])
-plt.plot(df[['index']], batch_size, 'go')
-ax = plt.subplot(1, 4, 3)
-ax.set_title('timesteps')
-plt.axis('normal')
-plt.yticks([1, 3, 6, 9, 12])
-plt.plot(df[['index']], look_back, 'ro')
 
-ax = plt.subplot(1, 4, 4)
+ax = plt.subplot(2, 4, 2)
+ax.set_title('batch_size')
+# plt.axis('normal')
+# plt.yticks([1, 2, 32, 64, 128, 256])
+plt.plot(df[['index']], batch_size, 'go')
+
+ax = plt.subplot(2, 4, 3)
+ax.set_title('timesteps')
+# plt.axis('normal')
+plt.plot(df[['index']], look_back, 'ro')
+#
+ax = plt.subplot(2, 4, 4)
 ax.set_title('Optmizers')
-# plt.yticks(['sgd', 'adam', 'rmsprop'])
 plt.plot(df[['index']], optimizers, 'yo')
+
+avg = numpy.polyfit(df[['index']].squeeze().tolist(), df[['loss']].squeeze().tolist(), 1)
+poly = numpy.poly1d(avg)
+
+ax = plt.subplot(2, 1, 2)
+ax.set_title('F1 Score')
+plt.plot(poly(df[['index']].squeeze().tolist()), zorder=10)
+ax = plt.subplot(2, 1, 2)
+plt.plot(df[['index']], df[['loss']])
 
 plt.tight_layout()
 plt.show()
