60ccef3b2f1156a985b95c81632036796b2557e6
Renato Avellar Nobre <rekanobre@gmail.com>
2020-03-23 22:21:23 -0300

    spinning up with env

--- testboard/models/DQLearning/ddpg_spin.py
+++ testboard/models/DQLearning/ddpg_spin.py
@@ -5,7 +5,6 @@ sys.path.append('../')
 # Necessário para importar os módulos
 import gym
 import datetime
-import gym_market
 import numpy as np
 from spinup import ddpg_tf1
 from tensorflow import keras
@@ -13,6 +12,11 @@ from spinup import ppo_pytorch
 from helpers.ddpg_input import DDPGInput
 from spinup.utils.run_utils import ExperimentGrid
 
+
+def env_fn():
+    import gym_market
+    return gym.make('MarketEnv-v0', n_insiders=3, start_money=1000,
+                    assets_prices=prices, insiders_preds=preds)
 
 # from keras.models import Sequential, Model
 # from keras.layers import Dense, Activation, Flatten, Input, Concatenate
@@ -27,16 +31,15 @@ log_dir = "../../../results/logdir/fit/" + datetime.datetime.now().strftime("%Y%
 
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
 #
-env = gym.make('MarketEnv-v0', n_insiders=3, start_money=1000,
-               assets_prices=prices, insiders_preds=preds)
+# env = gym.make('MarketEnv-v0', n_insiders=3, start_money=1000,
+#                assets_prices=prices, insiders_preds=preds)
 #
 # nb_actions = env.action_space.shape[0]
-
 eg = ExperimentGrid(name='ddpg-tf1-bench')
-eg.add('env', env, '', True)
+eg.add('env_fn', env_fn, '', True)
 # eg.add('seed', [10*i for i in range(args.num_runs)])
 eg.add('epochs', 10)
 eg.add('steps_per_epoch', 4000)
-eg.add('ac_kwargs:hidden_sizes', [(32,), (64,64)], 'hid')
+# eg.add('ac_kwargs:hidden_sizes', [(32,), (64,64)], 'hid')
 # eg.add('ac_kwargs:activation', [tf.tanh, tf.nn.relu], '')
 eg.run(ddpg_tf1, num_cpu=1)
