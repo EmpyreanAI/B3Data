5d9c986c15fb97a248e6f108094a18f4f26404b6
Renato Avellar Nobre <rekanobre@gmail.com>
2020-04-02 14:00:35 -0300

    Mudando parametros

--- testboard/models/DQLearning/ddpg_spin.py
+++ testboard/models/DQLearning/ddpg_spin.py
@@ -7,6 +7,7 @@ import gym
 import datetime
 import numpy as np
 from spinup import ddpg_tf1
+from spinup import sac_tf1
 from tensorflow import keras
 from helpers.ddpg_input import DDPGInput
 from spinup.utils.run_utils import ExperimentGrid
@@ -34,7 +35,7 @@ tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
 #                assets_prices=prices, insiders_preds=preds)
 #
 # nb_actions = env.action_space.shape[0]
-eg = ExperimentGrid(name='ddpg-tf1-bench')
+eg = ExperimentGrid(name='sac-tf1-bench')
 eg.add('env_fn', env_fn, '', True)
 eg.add('seed', 7)
 # eg.add('steps_per_epoch', 5000)
@@ -42,13 +43,13 @@ eg.add('epochs', 100)
 # eg.add('replay_size', 1000)
 # eg.add('gamma', 0.99)
 # eg.add('polyak', 0.3)
-eg.add('pi_lr', 0.000001)
-eg.add('q_lr', 0.000001)
-eg.add('batch_size', 1)
+# eg.add('pi_lr', 0.001)
+# eg.add('q_lr', 0.001)
+# eg.add('batch_size', 100)
 # eg.add('start_steps', 10000000)
 # eg.add('update_after', 700)
 # eg.add('update_every', 50)
-eg.add('act_noise', 0.3)
+# eg.add('act_noise', 0.3)
 # eg.add('num_test_episodes', 10)
 # eg.add('max_ep_len', 1000)
 # eg.add('save_freq', 3)
