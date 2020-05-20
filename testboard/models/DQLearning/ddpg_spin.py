2aed72d6c8c44a6f9f6111610c3dfce984ae323f
Renato Avellar Nobre <rekanobre@gmail.com>
2020-05-20 17:07:38 -0300

    n sei o que ta acontecendo

--- testboard/models/DQLearning/ddpg_spin.py
+++ testboard/models/DQLearning/ddpg_spin.py
@@ -15,7 +15,7 @@ import tensorflow as tf
 
 def env_fn():
     import gym_market
-    return gym.make('MarketEnv-v0', n_insiders=3, start_money=1000,
+    return gym.make('MarketEnv-v0', n_insiders=3, start_money=10000,
                     assets_prices=prices, insiders_preds=preds)
 
 # from keras.models import Sequential, Model
@@ -26,51 +26,44 @@ def env_fn():
 stockutil = StockUtil(['PETR3', 'VALE3', 'ABEV3'], [6, 6, 9])
 prices, preds = stockutil.prices_preds(start_year=2014, end_year=2014, period=11)
 
-print(len(prices[0]))
-print(len(preds[0]))
-
 # Get the environment and extract the number of actions.
 log_dir = "../../../results/logdir/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
 
-# env = gym.make('MarketEnv-v0', n_insiders=3, start_money=1000,
-#                assets_prices=prices, insiders_preds=preds)
-#
-# nb_actions = env.action_space.shape[0]
 """Tamanho do experimento = epocas*passos por epoca."""
 eg = ExperimentGrid(name='ddpg-tf1-bench')
 eg.add('env_fn', env_fn, '', True)
-eg.add('seed', 41)
+eg.add('seed', 21)
 """Passos por epoca."""
 # eg.add('steps_per_epoch', 5000)
 """Quantidade de epocas."""
-# eg.add('epochs', 3)
-# eg.add('replay_size', 5000)
+eg.add('epochs', 1000)
+eg.add('replay_size', int(1e8))
 """Fator de desconto, mais perto de zero mais poder para recomensas atuais
 mais perto de um mais prioridade pra recomensas futuras."""
-# eg.add('gamma', 0.7)
+# eg.add('gamma', 0.99)
 # eg.add('polyak', 0.995)
 """Taxa de aprendizagem da politica (ator)."""
-# eg.add('pi_lr', 0.001)
+# eg.add('pi_lr', 0.0001)
 """Taxa de aprendizagem do valor Q (critico)."""
-# eg.add('q_lr', 0.001)
+# eg.add('q_lr', 0.0001)
 """Quantidade de informacoes que é passada de uma só vez."""
-# eg.add('batch_size', 9)
+# eg.add('batch_size', 1024)
 """
 Enquanto o passo for menor que o valor (independe da epoca) acoes aleatorias.
 serao realizadas por motivos exploratorios
 """
-# eg.add('start_steps', 5000)
+eg.add('start_steps', 100000)
 """Updates atualizam: LossQ, QVals, LossPi."""
 """Quantas passos esperar antes de comecar a
 atualizar o gradientes descendente."""
-# eg.add('update_after', 2500)
+# eg.add('update_after', [2500,)
 """De quanto em quantos passos atualizar o gradiente, sem perder ratio."""
 # eg.add('update_every', 50)
 """Apos start_steps algoritmo usa a politica, mas
 exploracoes sao feitas via ruido."""
-# eg.add('act_noise', 0.1)
+# eg.add('act_noise', .1)
 """Numero de episodios de teste."""
 # eg.add('num_test_episodes', 10)
 """Caso o ambiente possa ficar perdido no episodio,
