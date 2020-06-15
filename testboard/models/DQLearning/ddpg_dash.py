6ef09042486b372879cd0656c40cfe1cc3eb2ec0
Khalil Carsten <khalilcarsten@gmail.com>
2020-06-15 15:29:59 -0300

    killing api

--- testboard/models/DQLearning/ddpg_dash.py
+++ testboard/models/DQLearning/ddpg_dash.py
@@ -6,7 +6,9 @@ import dash_html_components as html
 import plotly
 import pandas
 from dash.dependencies import Input, Output
-import urllib.request, json 
+import urllib.request, json
+import os
+import sys
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -71,10 +73,25 @@ def update_graph_live(n):
 
 
 def get_data():
-    with urllib.request.urlopen("http://localhost:5000/get_last_train") as url:
-        data = json.loads(url.read().decode())
+    progress = []
+    result = {}
+    for root, _, files in os.walk(os.path.join(sys.argv[1], 'data')):
+        for file in files:
+            if file.endswith(".txt"):
+                progress.append(os.path.join(root, file))
+
+    last_train = progress[-1]
+
+    columns = open(last_train).readlines()[0].split('\t')
+
+    for i in columns:
+        result[i] = []
+
+    for line in open(last_train).readlines()[1:]:
+        for i, value in enumerate(line.split('\t')):
+            result[columns[i]].append(float(value))
     
-    return pandas.DataFrame(data)
+    return pandas.DataFrame(result)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
